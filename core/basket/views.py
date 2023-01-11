1fe281a1e0082687cda19cc75eeb5e9f1eac2896
egorgargolin <98bariors@gmail.com>
2023-01-11 15:33:18 +0100

    + created basket and user sessions

---
+++ core/basket/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+
+from .basket import Basket
+from store.models import Product
+
+# Create your views here.
+
+def basket_summary(request):
+    basket = Basket(request)
+    return render(request, 'basket/summary.html', {'basket' : basket})
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product, qty=product_qty)
+
+        basketqty = basket.__len__()
+        response = JsonResponse({'qty': basketqty})
+        return response
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product=product_id)
+        response = JsonResponse({'Success' : True})
+        return response
--- core/store/views.py
+++ core/store/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Category, Product
 
-
 #def categories(request):
 #    return {'categories' : Category.objects.all()}
 
@@ -12,7 +11,7 @@ def product_all(request):
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'products/details.html', {'product' : product})
+    return render(request, 'products/product.html', {'product' : product})
 
 def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
